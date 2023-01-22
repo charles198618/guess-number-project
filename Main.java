285bd3854d0f3f24b5d2a475409cc2787cafe88e
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2023-01-22 15:39:39 -0500

    Adding the try catch block for Input validation

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 public class Main {
@@ -14,22 +15,29 @@ public class Main {
             count = 1;
             System.out.println("Well, "+name+", I am thinking of a number between 1 and 20.");
             System.out.println("Take a guess.");
-            int userInput = sc.nextInt();
-            while (userInput != randomValue) {
-                count++;
-                if (userInput > randomValue) {
-                    System.out.println("Your guess is too high.");
-                } else {
-                    System.out.println("Your guess is too low.");
-                }
-                System.out.println("Take a guess.");
-                userInput = sc.nextInt();
-            }
-            System.out.println("Good job, "+name+"! You guessed my number in " + count + " guesses!");
+            try {
+                int userInput = sc.nextInt();
 
-            System.out.println("Would you like to play again? (y or n)");
-            String playResponse = sc.next();
-            playerPlaying = !(playResponse.startsWith("N") || playResponse.startsWith("n"));
+                while (userInput != randomValue) {
+                    count++;
+                    if (userInput > randomValue) {
+                        System.out.println("Your guess is too high.");
+                    } else {
+                        System.out.println("Your guess is too low.");
+                    }
+                    System.out.println("Take a guess.");
+                    userInput = sc.nextInt();
+                }
+                    System.out.println("Good job, "+name+"! You guessed my number in " + count + " guesses!");
+
+                     System.out.println("Would you like to play again? (y or n)");
+                     String playResponse = sc.next();
+                     playerPlaying = !(playResponse.startsWith("N") || playResponse.startsWith("n"));
+            }
+            catch (InputMismatchException exception) {
+                System.out.println("You entered an Invalid input, please start over!");
+                break;
+            }
         }
     }
 }
